6d1725158cfbf03705acaf52296b167dbb9a8b8f
kittipong srikittichai <kittipong.sri@mail.kmutt>
2017-11-02 02:43:01 +0700

    add permission page

---
+++ mobile/src/components/Permission/Card.js
@@ -0,0 +1,74 @@
+import React from 'react'
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  Alert
+} from 'react-native'
+
+const IMAGE_URL = 'http://apollo.kmutt.ac.th/kmuttstdpic/default.aspx?&stdcode='
+
+export const styles = StyleSheet.create({
+  container: {
+    width: '48%',
+    height: 250,
+    backgroundColor: '#162f52',
+    borderRadius: 12,
+    marginBottom: 12,
+    padding: 16,
+    alignItems: 'center'
+  },
+  cardTitle: {
+    color: 'white',
+    fontFamily: 'BoonJot-Regular',
+    fontSize: 26,
+    margin: 5
+  },
+  cardImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 100,
+    margin: 5
+  },
+  cardContent: {
+    color: 'white',
+    fontFamily: 'BoonJot-Regular',
+    fontSize: 28,
+    margin: 5
+  },
+  exit: {
+    position: 'absolute',
+    left: 150,
+    top: 10
+  },
+  exitText: {
+    color: '#ccc',
+    fontFamily: 'BoonJot-Regular',
+    fontSize: 15
+  }
+})
+
+export const Card = ({ detail }) => (
+  <View style={styles['container']}>
+    <TouchableOpacity
+      style={styles['exit']}
+      onPress={() =>
+        Alert.alert('Are you sure you want to remove this person?', '', [
+          { text: 'cancel', onPress: () => null },
+          { text: 'yes', onPress: () => null }
+        ])}
+    >
+      <Text style={styles['exitText']}>X</Text>
+    </TouchableOpacity>
+    <Text style={styles['cardTitle']}>{detail.name}</Text>
+    <Image
+      style={styles['cardImage']}
+      source={{ uri: `${IMAGE_URL}${detail.studentId}` }}
+    />
+    <Text style={styles['cardContent']}>01:06:04</Text>
+  </View>
+)
+
+export default Card
--- mobile/src/components/Permission/index.js
+++ mobile/src/components/Permission/index.js
@@ -1,12 +1,60 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, StyleSheet, ScrollView } from 'react-native'
+import ActionButton from 'react-native-action-button'
 
 import App from '../App'
+import Card from './Card'
+
+export const styles = StyleSheet.create({
+  container: {
+    padding: 18,
+    flex: 1
+  },
+  titleText: {
+    color: 'white',
+    fontSize: 30,
+    fontFamily: 'BoonJot-Regular',
+    marginBottom: 15
+  },
+  cardsContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    flexWrap: 'wrap',
+    paddingBottom: 15
+  },
+  addButton: {
+    position: 'absolute',
+    bottom: 0
+  },
+  addIcon: {
+    width: 60,
+    height: 60
+  }
+})
+
+const mockData = [
+  { name: 'Jerrylee', studentId: '59130500123', until: '9:23 PM' },
+  { name: 'Stefa', studentId: '57130500024', until: '3:30 PM' },
+  { name: 'Eric', studentId: '6013050064', until: '7:09 PM' },
+  { name: 'Jud', studentId: '58130500040', until: '9:48 PM' },
+  { name: 'Saw', studentId: '56130500017', until: '9:14 PM' }
+]
 
 export const Index = () => (
-  <View>
-    <Text>this is permission component</Text>
-  </View>
+  <ScrollView style={styles['container']}>
+    <Text style={styles['titleText']}>Permission</Text>
+    <View style={styles['cardsContainer']}>{renderCards(mockData)}</View>
+    <ActionButton
+      buttonColor='red'
+      useNativeFeedBack={false}
+      offsetY={320}
+      onPress={() => null}
+    />
+  </ScrollView>
 )
+
+export const renderCards = datas =>
+  datas.map((data, index) => <Card key={index} detail={data} />)
 
 export default App(Index)
